4a98f8a6256145e515a79a397b8c2562cc55dd9f
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-05-15 12:23:44 -0600

    buffer ring tests working

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,16 +2,15 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = []
-        self.front = 0
+        self.current = 0
     
     def append(self, item):
-        # if queue is empty
 
         # if queue is full/at capacity
         if len(self.data) == self.capacity:
-            self.data.pop(self.front)
-            self.data.insert(self.front, item)
-            self.front = (self.front + 1) % self.capacity
+            self.data.pop(self.current)
+            self.data.insert(self.current, item)
+            self.current = (self.current + 1) % self.capacity
         else:
             self.data.append(item)
         
@@ -19,7 +18,7 @@ class RingBuffer:
         return self.data
 
 
-buf = RingBuffer(3)
-for i in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+buf = RingBuffer(5)
+for i in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']:
     buf.append(i)
     print(buf.get())
