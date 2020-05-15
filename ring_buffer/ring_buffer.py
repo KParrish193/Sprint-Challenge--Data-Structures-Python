851607208cb2774f479d26bc95b73a70aa3d3547
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-05-15 12:11:17 -0600

    lunch time commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.data = []
+        self.front = 0
+    
     def append(self, item):
-        pass
+        # if queue is empty
 
-    def get(self):
-        pass
+        # if queue is full/at capacity
+        if len(self.data) == self.capacity:
+            self.data.pop(self.front)
+            self.data.insert(self.front, item)
+            self.front = (self.front + 1) % self.capacity
+        else:
+            self.data.append(item)
+        
+    def get(self): 
+        return self.data
+
+
+buf = RingBuffer(3)
+for i in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+    buf.append(i)
+    print(buf.get())
